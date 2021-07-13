410eff3b4ab56597bd6bf4d552d534a86d35fe94
小坂和也 <shinozu1316@icloud.com>
2021-07-13 14:44:56 +0900

    first-commit

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import List from '../views/List.vue'
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
+import Detail from '../views/Detail.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/list',
+    name: 'List',
+    component: List
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/detail',
+    name: 'Detail',
+    component: Detail,
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+})
+
+export default router
---
+++ src/views/Detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h2>api</h2>
+  </div>
+</template>
+<script>
+export default {
+  created() {
+    this.$axios
+      .get("/public/v1/ticker")
+      .then((response) => console.log(response));
+  },
+};
+</script>
